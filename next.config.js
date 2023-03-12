446fb7577110202f91fc72414f72e7fba4de534b
Sebastian Remm <sebastian.remm@web.de>
2023-03-12 19:13:33 +0100

    - added "de" as default language to <html>

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     deviceSizes: [82, 110, 140, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+  },
+  i18n: {
+    locales: ["de"],
+    defaultLocale: "de"
   }
 }
 
