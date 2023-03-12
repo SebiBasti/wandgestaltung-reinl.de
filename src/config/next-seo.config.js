804a267533e9a72d64fcf03b0e4865aff559fc53
Sebastian Remm <sebastian.remm@web.de>
2023-03-12 19:08:30 +0100

    - added next-seo - updated favicon

---
+++ src/config/next-seo.config.js
@@ -0,0 +1,22 @@
+export default {
+  title: 'Wandgestaltung Reinl',
+  description: 'Werte schaffen, Werte erhalten - Exklusive Malerarbeiten seit 2004',
+  canonical: 'https://www.wangestaltung-reinl.de/',
+  openGraph: {
+    type: 'website',
+    locale: 'de_DE',
+    url: 'https://www.wandgestaltung-reinl.de/',
+    title: 'Wandgestaltung Reinl',
+    site_name: 'Wandgestaltung Reinl - Werte schaffen, Werte erhalten',
+    description: 'Werte schaffen, Werte erhalten - Exklusive Malerarbeiten seit 2004',
+    images: [
+      {
+        url: 'https://www.wandgestaltung-reinl.de/images/misc/og_meta_image.png',
+        type: 'image/png',
+        width: 1200,
+        height: 627,
+        alt: 'wandgestaltung-reinl.de og metatag Bild',
+      },
+    ],
+  },
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.scss'
+import { DefaultSeo } from 'next-seo'
+import SEO from '@/config/next-seo.config'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo {...SEO} />
+      <Component {...pageProps} />
+    </>
+  )
 }
