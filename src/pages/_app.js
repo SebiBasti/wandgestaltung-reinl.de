32fb66633de55b0df5caa90b290f5de01aa3b02d
Sebastian Remm <sebastian.remm@web.de>
2023-03-12 22:15:18 +0100

    - added redirect for old paths

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,1 @@
-import '@/styles/globals.scss'
-import { DefaultSeo } from 'next-seo'
-import SEO from '@/config/next-seo.config'
-
-export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <DefaultSeo {...SEO} />
-      <Component {...pageProps} />
-    </>
-  )
-}
+import '@/styles/globals.scss'import { DefaultSeo } from 'next-seo'import SEO from '@/config/next-seo.config'export default function App({ Component, pageProps }) {  return (    <>      <DefaultSeo {...SEO} />      <Component {...pageProps} />    </>  )}
