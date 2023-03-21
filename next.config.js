4d489bb5c4ea3890b47399db4db43e1c8b736099
Sebastian Remm <sebastian.remm@web.de>
2023-03-21 04:24:28 +0100

    - testing edge runtime

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    runtime: 'experimental-edge',
+  },
   reactStrictMode: true,
   images: {
     deviceSizes: [82, 110, 140, 640, 750, 828, 1080, 1200, 1920, 2048, 3840]
